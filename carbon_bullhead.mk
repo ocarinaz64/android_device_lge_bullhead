cbf6b3131abff5d53d4987cb6cb8edeccd7bb32a
Michael Schneider <schneidermichael@gmail.com>
2017-10-04 20:30:12 -0500

    Update build fingerprint

--- carbon_bullhead.mk
+++ carbon_bullhead.mk
@@ -35,5 +35,5 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.2/N2G48C/4104010:user/release-keys" \
-    PRIVATE_BUILD_DESC="bullhead-user 7.1.2 N2G48C 4104010 release-keys"
+    BUILD_FINGERPRINT="google/bullhead/bullhead:8.0.0/OPR4.170623.006/4286358:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 8.0.0 OPR4.170623.006 4286358 release-keys"
